661cac1d753a03d3153ea3b9f113cb93207be9cc
Zaheer_Ahmad <zaheerahmadlangrial92@gmail.com>
2024-05-15 01:14:48 +0500

    Signin SignUp Api complete

--- Backend/src/controller/user.controller.js
+++ Backend/src/controller/user.controller.js
@@ -1,14 +1,42 @@
 import User from "../model/user.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+export const signUp = async (req, res) => {
+  console.log("request", req.body);
+  const { fullname, email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    return res
+      .status(400)
+      .json({ message: "Your account is already existing" });
+  }
+  const hashPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({
+    fullname: fullname,
+    email: email,
+    password: hashPassword,
+  });
+  try {
+    newUser.save();
+    res.status(200).json({ message: "Account Created Successfull" });
+  } catch (error) {
+    console.log("Error", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-export const signUp = async (req , res) => {
-    console.log('request' , req.body)
-    const {fullname , email , password} = req.body;
+export const signIn = async (req , res) => {
+    const {email , password} = req.body;
     try {
-        const user = await User.findOne({email})
-        if(!user) {
-            return res.status(400).json({message : 'Your account is already existing'})
-        }
+        const validUser = await User.findOne({email})
+        if(!validUser) return res.status(404).json({message : 'User not found'})
+        const validPassword = bcryptjs.compareSync(password , validUser.password);
+        if(!validPassword) return res.status(401).json({message : 'Invalid password'})
+        const token = jwt.sign({id : validUser._id} , process.env.JWT_SCRET)
+        const {password : hashPassword , ...rest} = validUser._doc
+        const expairyDate = new Date(Date.now() + 3600000)
+        res.cookie('access token' , token , {httpOnly : true , expires : expairyDate }).status(200).json(rest)
     } catch (error) {
         console.log('Error' , error)
     }
--- Backend/src/route/user.route.js
+++ Backend/src/route/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { signUp } from '../controller/user.controller.js';
+import { signIn, signUp } from '../controller/user.controller.js';
 
 const router = express.Router();
 
 router.post('/signup' , signUp)
+router.post('/signin' , signIn)
 
 export default router
